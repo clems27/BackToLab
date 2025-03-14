9f59ab5c35bbe5a114e64aa4c0fc0719ca8cf95d
Thivzo <thiviyanp16@hotmail.com>
2025-03-14 14:51:38 +0000

    dyanmic

--- app/app.js
+++ app/app.js
@@ -5,6 +5,8 @@ const path = require("path");
 // Create an Express application
 const app = express();
 
+//const db = require('./services/db/sd2-db');
+
 // Serve static files (CSS, images, etc.) from the "src" folder
 app.use(express.static(path.join(__dirname, "..", "src")));
 
@@ -20,13 +22,60 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+
+
 /*
-  Route: Greeting (Optional)
-  A simple dynamic route to test passing parameters.
+  TEST ROUTE WITH DB
 */
-app.get("/hello/:name", (req, res) => {
-  res.send(`Hello ${req.params.name}!`);
+
+/*
+app.get("/all-users", function(req, res) {
+  var sql = 'select * from users'
+  var output = '<table border="1px">';
+  db.query(sql).then(results => {
+
+    res.json(results);
+  })
+  res.send("Hello All users");
+
+}); 
+
+*/
+
+/*
+  DYNAMIC ROUTE FOR USERS
+*/
+/*
+  app.get("/users/:id", function(req, res) {
+  var userID = req.params.id;
+  var sql = 'SELECT * FROM user'
+
+  db.query(sql, [userID]).then(results => {
+    res.render(results);
+  })
+
 });
+*/
+
+
+/*
+  DYNAMIC ROUTE FOR RECIPES
+*/
+
+/*
+  app.get("/recipes/:id", function(req, res) {
+  var recipeID = req.params.id;
+  var sql = 'SELECT * FROM recipes'
+
+  db.query(sql, [recipeID]).then(results => {
+    res.render(results);
+  })
+
+});
+*/
+
+
+
 
 // Start the server on port 3000 and log a message once it is running
 const PORT = 3000;
